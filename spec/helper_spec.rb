5ab65e7d6547c603923fc872fbf9ca8ac1100eb9
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-01 16:37:45 +0900

    test: add tests for users()

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -4,6 +4,45 @@ describe Codeforces::Helper, :vcr => true do
 
   let!(:client) { Codeforces::Client.new }
 
+  describe "#users" do
+
+    context "users" do
+      subject! { client.users }
+      it { should_not be_empty }
+    end
+
+    context "users.grep :country => Russia" do
+      subject! { client.users.grep :country => "Russia" }
+      it { expect(subject.map &:handle).to include "Petr" }
+      it { expect(subject.map &:handle).to_not include "tourist" }
+    end
+
+    context "users.grep :country => /Russia/" do
+      subject! { client.users.grep :country => /^Russia$/ }
+      it { expect(subject.map &:handle).to include "Petr" }
+      it { expect(subject.map &:handle).to_not include "tourist" }
+    end
+
+    context "users.grep :country => Belarus" do
+      subject! { client.users.grep :country => "Belarus" }
+      it { expect(subject.map &:handle).to_not include "Petr" }
+      it { expect(subject.map &:handle).to include "tourist" }
+    end
+
+    context "users.grep :country => /Belarus/" do
+      subject! { client.users.grep :country => /^Belarus$/ }
+      it { expect(subject.map &:handle).to_not include "Petr" }
+      it { expect(subject.map &:handle).to include "tourist" }
+    end
+
+    context "users.grep :organization => Google" do
+      subject! { client.users.grep :organization => "Google" }
+      it { expect(subject.map &:handle).to include "Petr" }
+      it { expect(subject.map &:handle).to_not include "tourist" }
+    end
+
+  end # users
+
   describe "#each_contest" do
 
     example "it is called 450 or more times" do
@@ -28,7 +67,7 @@ describe Codeforces::Helper, :vcr => true do
       it { should eq "Russia" }
     end
 
-    context "user(DmitriyH).submissions", :current => true do
+    context "user(DmitriyH).submissions" do
       subject! { client.user("DmitriyH").submissions }
       it { should be_a Array }
     end
