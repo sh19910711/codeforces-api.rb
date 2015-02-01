a8ac7fb437eef8cc7c1d55a5754d38724330b7a4
Hiroyuki Sano <sh19910711@gmail.com>
2015-02-01 16:59:20 +0900

    helper: add contest()

--- lib/codeforces/helper.rb
+++ lib/codeforces/helper.rb
@@ -1,7 +1,22 @@
 module Codeforces::Helper
 
+  def contest(query)
+    if query.is_a?(Integer)
+      contests.select {|c| query === c.id }.first
+    else
+      ret = contests.select {|c| query === c.name }
+      raise "Error: contest is not found" if ret.empty?
+      raise "Error: query is ambiguous" if ret.length > 1
+      ret.first
+    end
+  end
+
+  def contests
+    api.contest.list
+  end
+
   def each_contest
-    api.contest.list.each {|contest| yield(contest) if block_given? }
+    contests.each {|contest| yield(contest) if block_given? }
   end
 
   def user(handle)
@@ -12,8 +27,12 @@ module Codeforces::Helper
     create_users api.user.rated_list.map {|u| create_user u }
   end
 
+  def recent_status
+    api.problemset.recent_status.map {|s| create_submission s }
+  end
+
   def each_status
-    api.problemset.recent_status.map {|s| create_submission s }.each {|status| yield(status) if block_given? }
+    recent_status.each {|status| yield(status) if block_given? }
   end
 
 end
